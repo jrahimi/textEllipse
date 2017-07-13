0eb27ef9d684b1ea582630f9085f9a849a530684
Julianne Rahimi <jules386@gmail.com>
2017-07-13 14:22:35 +0100

    Joel rewrote with classes and includes speech - UPDATE
    Joel rewrote with classes and includes speech

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,210 +1,144 @@
-var xpos, ypos, xpos2, ypos2 = []; // Starting position of shape  
+//Content
+var message = ["Miss *** that smile.", 
+"Hurry up and move here.", 
+"It was global disaporta.", 
+"What do you have going on this weekend?.", 
+"Same I'm going out tomorrow.", 
+"What are you up to Saturday night? I'm down to get drinks.", 
+"Where do you live?", "What do you do?", 
+"That's amazing! What kind of music do you guys play?", 
+"It's similar to Maroon 5, but a little more urban if that makes sense lol."]; 
 
-var xspeed = 1.8; // Speed of the shape
-var yspeed = 1.2; // Speed of the shape
+var message2 = ["What kind of dog do you have?", 
+"So this will have to be a short bu furious romance ;)", 
+"I'm a writer.", 
+"So what are your plans for later?", 
+"Well I was hoping to meet a charming young photographer for a drink.", 
+"So what do we do now? (: x", 
+"Right so where do you know that you can take me for a drink?", 
+"Haha OK not too intimidating! Do you want to meet up for a drink?", 
+"Really close friend of mine in IV. I actually have his name tattooed on me haha.", 
+"Holy shit I totally remember that!"];
 
-var xdirection, ydirection, xdirection2, ydirection2; //Direction of the shape
+var theVoice = new p5.Speech('Google UK English Male'); // new P5.Speech object
+theVoice.onStart = startSpeaking;
+theVoice.onEnd = endSpeaking;
+var speaking = false;
 
-//Text
-var m, m2 = [];
-var message = ["Miss *** that smile.",
-  "Hurry up and move here.",
-  "It was global disaporta.",
-  "What do you have going on this weekend?.",
-  "Same I'm going out tomorrow.",
-  "What are you up to Saturday night? I'm down to get drinks.",
-  "Where do you live?", "What do you do?",
-  "That's amazing! What kind of music do you guys play?",
-  "It's similar to Maroon 5, but a little more urban if that makes sense lol."
-];
-
-var message2 = ["What kind of dog do you have?",
-  "So this will have to be a short bu furious romance ;)",
-  "I'm a writer.",
-  "So what are your plans for later?",
-  "Well I was hoping to meet a charming young photographer for a drink.",
-  "So what do we do now? (: x",
-  "Right so where do you know that you can take me for a drink?",
-  "Haha OK not too intimidating! Do you want to meet up for a drink?",
-  "Really close friend of mine in IV. I actually have his name tattooed on me haha.",
-  "Holy shit I totally remember that!"
-];
-
-//new p5.Font ([f]); 
-
-var len, r, len2, r2; //Calculates the radius of the text ellipse
-
-var value = 0;
-
-var myVoice = new p5.Speech(); // new P5.Speech object
-var iptr = 0; // a counter for the words
+var conversation1;
+var conversation2;
 
 function setup() {
   createCanvas(800, 800);
-  //fullScreen();
-  //frameRate(120);
 
-  xpos = new Array(message.length);
-  ypos = new Array(message.length);
+  textFont("Times");
+  textSize(20);
+  textAlign(CENTER);
+  smooth();
+  var conversation1Colour = color(255,255,255);
+  var conversation2Colour = color(255,0,0);
 
-  xpos2 = new Array(message2.length);
-  ypos2 = new Array(message2.length);
-
-  xdirection = new Array(message.length);
-  ydirection = new Array(message.length);
-
-  xdirection2 = new Array(message2.length);
-  ydirection2 = new Array(message2.length);
-
-  for (var i = 0; i < message.length; i++) {
-    xpos[i] = random(width / 2);
-    ypos[i] = random(height / 2);
-
-    xdirection[i] = random(1);
-    ydirection[i] = random(1);
-
-    m = message[i];
-  }
-
-  for (var j = 0; j < message2.length; j++) {
-    xpos2[j] = random(width / 2);
-    ypos2[j] = random(height / 2);
-
-    xdirection2[j] = random(1);
-    ydirection2[j] = random(1);
-
-    m2 = message2[j];
-  }
+  conversation1 = new Conversation(message, conversation1Colour);
+  conversation2 = new Conversation(message2, conversation2Colour);
 }
 
 function draw() {
   background(0);
 
-  for (var i = 0; i < message.length; i++) {
-    move(i);
-    display(i);
-    m = message[i];
-  }
+  conversation1.update();
+  conversation2.update();
 
-  for (var j = 0; j < message2.length; j++) {
-    move2(j);
-    display2(j);
-    m2 = message2[j];
+  conversation1.draw();
+  conversation2.draw();
+}
+
+function mousePressed(){
+  if(!speaking){ //only speak if we are not already speaking
+    //choose which conversation we want to start with
+    var randomNumber = random();
+    console.log("The random number is: "+randomNumber);
+    if(randomNumber > 0.5){
+      console.log("Conversation 1 is about to say something");
+      saySomething(conversation1);
+    }else{
+      console.log("Conversation 2 is about to say something");
+      saySomething(conversation2);
+    }
   }
 }
 
-function move(i) {
-  xpos[i] += (xspeed * xdirection[i]);
-  ypos[i] += (yspeed * ydirection[i]);
+function saySomething(aConversation){
+  var randomIndexToThingToSay = Math.floor(random(aConversation.messages.length));
+  var thingToSay = aConversation.messages[randomIndexToThingToSay];
 
-  if (xpos[i] > width - r || xpos[i] < r) {
-    xdirection[i] *= -1;
-  }
-  if (ypos[i] > height - r || ypos[i] < r) {
-    ydirection[i] *= -1;
-  }
-}
-
-function move2(i) {
-  xpos2[i] += (xspeed * xdirection2[i]);
-  ypos2[i] += (yspeed * ydirection2[i]);
-
-  if (xpos2[i] > width - r2 || xpos2[i] < 0) {
-    xdirection2[i] *= -1;
-  }
-  if (ypos2[i] > height - r2 || ypos2[i] < 0) {
-    ydirection2[i] *= -1;
-  }
-}
-
-function display(i) {
-  //f = textFont("Times", 20, true);
-  //textFont(f);
-  textFont("Times");
-  textSize(20);
-  textAlign(CENTER);
-  smooth();
-
-  var w;
-  var arclength = 0;
-
-  var xp = xpos[i];
-  var yp = ypos[i];
-
-  for (var j = 0; j < m.length; j++) {
-    //text += possible.charAt(Math.floor(Math.random() * possible.length));
-    len = textWidth(m);
-    r = (len + 5) / (2 * PI); //equation for radius
-
-    var currentChar = m.charAt(j);
-    w = textWidth(currentChar);
-    arclength += w / 2;
-    var theta = PI + arclength / r;
-
-    push();
-    translate((r * cos(theta)) + xp, (r * sin(theta)) + yp);
-    rotate(theta + PI / 2);
-    fill(255);
-    text(currentChar, 0, 0);
-    pop();
-
-    arclength += w / 2;
-  }
-
-  if (mouseX > xpos[i] && mouseX < xpos[i] + r &&
-    mouseY > ypos[i] && mouseY < ypos[i] + r) {
-    xdirection[i] *= -1;
-    ydirection[i] *= -1;
-  }
-}
-
-function display2(i) {
-  //f = textFont("Times", 20, true);
-  //textFont(f);
-  textFont("Times");
-  textSize(20);
-  textAlign(CENTER);
-  smooth();
-
-  var w;
-  var arclength = 0;
-
-  var xp = xpos2[i];
-  var yp = ypos2[i];
-
-  for (var j = 0; j < m2.length; j++) {
-    len2 = textWidth(m2);
-    r2 = (len2 + 5) / (2 * PI); //equation for radius
-
-    var currentChar = m2.charAt(j);
-    w = textWidth(currentChar);
-    arclength += w / 2;
-    var theta = PI + arclength / r2;
-
-    push();
-    translate((r2 * cos(theta)) + xp, (r2 * sin(theta)) + yp);
-    rotate(theta + PI / 2);
-    fill(255, 0, 0);
-    text(currentChar, 0, 0);
-    pop();
-
-    arclength += w / 2;
-  }
-
-  function mouseIsPressed() {
-    // if in bounds:
-    //if(mousePressed) {
-    if (mouseX < width && mouseY < height) {
       // randomize voice and speak word:
-      myVoice.setVoice(Math.floor(random(myVoice.voices.length)));
-      // myVoice.speak(message[iptr]);
-      myVoice.speak('hello');
-      iptr = (iptr + 1) % message.length; // increment
+  //theVoice.setVoice(Math.floor(random(theVoice.voices.length)));
+  console.log("I'm going to say "+thingToSay);
+  theVoice.speak(thingToSay);
+}
+
+function startSpeaking(){
+  console.log("I'm starting to speak");
+  speaking = true;
+}
+
+function endSpeaking(){
+  console.log("I've finished speaking");
+  speaking = false;
+}
+
+function Conversation(someMessages, aColour){ //https://github.com/processing/p5.js/wiki/JavaScript-basics#using-parameters for how to make classes and constructors
+  this.messages = someMessages;
+  this.colour = aColour;
+  this.positions = new Array(this.messages.length);
+  this.velocities = new Array(this.messages.length);
+
+  for(var i=0; i < this.messages.length; i++){
+    this.positions[i] = createVector(random(width/2), random(height/2)); //https://p5js.org/reference/#/p5.Vector
+    this.velocities[i] = createVector(random(1), random(1)); //https://p5js.org/reference/#/p5/random
+  }
+
+  this.update = function(){
+    for(var i=0; i < this.messages.length; i++){ //for all the messages
+      this.positions[i].add(this.velocities[i]); //add the velocity to the position to move it correctly on this frame
+
+      if(this.positions[i].x > width || this.positions[i].x < 0){
+        //if the x position is to the left of the screen or to the right, flip the velocity so that it bounces back into view
+        this.velocities[i].x *= -1;
+      }
+
+      if(this.positions[i].y > height || this.positions[i].y < 0){
+        //if the x position is to the left of the screen or to the right, flip the velocity so that it bounces back into view
+        this.velocities[i].y *= -1;
+      }
     }
   }
 
-  //if (mouseX > xpos2[i] && mouseX < xpos2[i] + r || mouseY > ypos2[i] && mouseY < ypos2[i] + r) {
-  //  xp = mouseX;
-  //  yp = mouseY;
-  //}
+  this.draw = function(){
+    for(var i=0; i < this.messages.length; i++){
+      var currentMessage = this.messages[i]; //get the ith message
+      //console.log("We are looking at this message: "+message);
+      var positionOfMessage = this.positions[i]; //get the ith position
+      var widthOfMessage = textWidth(currentMessage);
+      var radiusOfMessage = (widthOfMessage + 5)/(2*PI); //equation for radius, 5 is added for a bit of padding
+      var arcLength = 0;
+
+      for (var j=0; j < currentMessage.length; j++) {
+        var currentCharacter = currentMessage.charAt(j);
+        var characterWidth = textWidth(currentCharacter);
+
+        arcLength += characterWidth/2; //move along the arc, half the width of the character at a time
+        var theta = PI + arcLength / radiusOfMessage; //theta (angle of change) is equal to PI (offset) + arclength
+
+        push();
+          translate((radiusOfMessage*cos(theta)) + positionOfMessage.x, (radiusOfMessage*sin(theta)) + positionOfMessage.y);
+          rotate(theta+PI/2);
+          fill(this.colour);
+          text(currentCharacter, 0, 0);
+        pop();
+
+        arcLength += characterWidth/2; //move along the arc, half the width of the character at a time
+      }
+    }
+  }
 }
